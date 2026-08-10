b03c7d029185b2543bd40533cc8ad27825e55f87
이선학 <mumahub1219@gmail.com>
2026-08-10 18:32:11 +0900

    [feat] 맵 매니저, 맵 스크립트 제작

---
+++ Assets/Scripts/Features/Map/BaseMap.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BaseMap : MonoBehaviour
+{
+    [Header("타일맵")]
+    [SerializeField] private Grid Grid;
+    [SerializeField] private Tilemap Tilemap_Ground;
+    [SerializeField] private Tilemap Tilemap_CraftArea;
+
+    [Header("포인트 좌표")]
+    [SerializeField] private Transform Transform_startPoint;
+    [SerializeField] private Transform Transform_arrivePoint;
+    [SerializeField] private Transform Transform_spawnPoint;
+
+    private List<GameObject> placedSegment = new List<GameObject>();
+    public Vector3 StartPosition => Transform_startPoint.position;
+    public Vector3 ArrivePosition => Transform_arrivePoint.position;
+
+    private void Start()
+    {
+        if (Tilemap_CraftArea != null)
+        {
+            TilemapRenderer renderer = Tilemap_CraftArea.GetComponent<TilemapRenderer>();
+            if (renderer != null)
+            {
+                renderer.enabled = false;
+            }
+        }
+    }
+
+    public Vector3Int WorldToCell(Vector3 worldPosition)
+    {
+        return Grid.WorldToCell(worldPosition);
+    }
+
+    public Vector3 GetCellCenterWorld(Vector3Int cellPosition)
+    {
+        return Grid.GetCellCenterWorld(cellPosition);
+    }
+
+    public bool CanBuild(Vector3 worldPosition)
+    {
+        Vector3Int cellPos = WorldToCell(worldPosition);
+
+        if (Tilemap_Ground != null && Tilemap_Ground.HasTile(cellPos))
+        {
+            return false;
+        }
+
+        if (Tilemap_CraftArea != null && !Tilemap_CraftArea.HasTile(cellPos))
+        {
+            return false;
+        }
+
+        foreach (var obj in placedSegment)
+        {
+            if ( obj != null && WorldToCell(obj.transform.position) == cellPos)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void RegisterObject(GameObject obj)
+    {
+        placedSegment.Add(obj);
+    }
+
+    public void ClearAllPlacedObjects()
+    {
+        foreach (var obj in placedSegment)
+        {
+            if (obj != null)
+            {
+                Destroy(obj);
+            }
+        }
+        placedSegment.Clear();
+    }
+
+    public void SetCraftAreaVisibility(bool isVisible)
+    {
+        if (Tilemap_CraftArea != null)
+        {
+            var renderer = Tilemap_CraftArea.GetComponent<TilemapRenderer>();
+            if (renderer != null)
+            {
+                renderer.enabled = isVisible;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/MapManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : SingletonBase<MapManager>
+{
+    [SerializeField] private GameObject Prefab_baseMap;
+
+    [SerializeField] private Vector3 firstMapSpawnPosition = Vector3.zero;
+    [SerializeField] private float mapDistanceOffset = 2.0f;
+
+    private List<BaseMap> activeMaps = new List<BaseMap>();
+    public int MapCount => activeMaps.Count;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    public void GenerateFullLevel(int mapCount)
+    {
+        ClearAllMaps();
+
+        Vector3 nextSpawnPos = firstMapSpawnPosition;
+
+        for (int i = 0; i < mapCount; i++)
+        {
+            GameObject mapObj = Instantiate(Prefab_baseMap, nextSpawnPos, Quaternion.identity, transform);
+            BaseMap baseMap = mapObj.GetComponent<BaseMap>();
+
+            if (baseMap != null)
+            {
+                activeMaps.Add(baseMap);
+
+                float mapWidth = baseMap.ArrivePosition.x - baseMap.StartPosition.x;
+                nextSpawnPos += new Vector3(mapWidth + mapDistanceOffset, 0, 0);
+            }
+        }
+    }
+
+    public BaseMap GetMap(int index)
+    {
+        if (index >= 0 && index < activeMaps.Count)
+        {
+            return activeMaps[index];
+        }
+        return null;
+    }
+
+    public Vector3 GetGlobalStartPosition()
+    {
+        if (activeMaps.Count > 0 && activeMaps[0] != null)
+        {
+            return activeMaps[0].StartPosition;
+        }
+        return Vector3.zero;
+    }
+
+    public void ClearAllMaps()
+    {
+        foreach (var map in activeMaps)
+        {
+            if (map != null)
+            {
+                Destroy(map.gameObject);
+            }
+        }
+        activeMaps.Clear();
+    }
+}
