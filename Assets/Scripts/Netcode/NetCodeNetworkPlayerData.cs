f4f04f031813244f1ec04f3272234d47948eee8d
김현빈 <khbkss77@naver.com>
2026-08-11 17:23:12 +0900

    [feat] 테스트용 로비 UI / 네트워크 저장용 플레이어 데이터구조체 추가

---
+++ Assets/Scripts/Netcode/NetCodeNetworkPlayerData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Netcode;
+using UnityEngine;
+
+public struct NetCodeNetworkPlayerData : INetworkSerializable, IEquatable<NetCodeNetworkPlayerData>
+{
+    public ulong ClientId;               
+    public bool IsReady;                 
+
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        serializer.SerializeValue(ref ClientId);
+        serializer.SerializeValue(ref IsReady);
+    }
+
+    public bool Equals(NetCodeNetworkPlayerData other)
+    {
+        return ClientId == other.ClientId &&
+               IsReady == other.IsReady;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is NetCodeNetworkPlayerData other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(ClientId, IsReady);
+    }
+}
---
+++ Assets/Scripts/Netcode/Test/TestLobbyUI.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestLobbyUI : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField InputField_Name;
+    [SerializeField] private TMP_InputField InputField_RoomCode;
+    [SerializeField] private Button Button_Host;
+    [SerializeField] private Button Button_Client;
+
+    private void Start()
+    {
+        Button_Host.onClick.AddListener(OnClickHostButton);
+        Button_Client.onClick.AddListener(OnClickClientButton);
+    }
+
+    private void OnClickHostButton()
+    {
+        NetCodeNetworkManager.Inst.StartAsHost();
+    }
+
+    private void OnClickClientButton()
+    {
+        NetCodeNetworkManager.Inst.StartAsClient();
+    }
+
+    private void SyncPlayerName()
+    {
+        string playerName = string.Empty;
+        if(InputField_Name != null && !string.IsNullOrEmpty(InputField_Name.text))
+        {
+            playerName = InputField_Name.text;
+        }
+        else
+        {
+            playerName = $"Player_{Random.Range(100, 999)}";
+        }
+
+        Debug.Log($"설정된 닉네임 : {playerName}");
+    }
+}
