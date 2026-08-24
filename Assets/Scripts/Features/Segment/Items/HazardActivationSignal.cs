0d37f85ac2addb01edc76cf791f22682c9f6a50d
이강호 <tiger04176672@gmail.com>
2026-08-24 15:04:30 +0900

    [chore] 배치 아이템 1종 추가 (SwingHammer)

---
+++ Assets/Scripts/Features/Segment/Items/HazardActivationSignal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+// 장애물을 공통으로 구독하는 스크립트. 장애물이 추가될 때 공통으로 이 이벤트를 구독하여 유지보수 보강
+public static class HazardActivationSignal
+{
+    public static event Action OnActivateAllRequested;
+
+    public static void ActivateAll()
+    {
+        OnActivateAllRequested?.Invoke();
+    }
+}
