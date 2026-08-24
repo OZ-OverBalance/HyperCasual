3966268ba9c9733b85bd6e3bb0a73bd7d4a582e1
이강호 <tiger04176672@gmail.com>
2026-08-24 15:54:48 +0900

    [feat] 밟으면 무너지는 발판용 스크립트 제작

---
+++ Assets/Scripts/Features/Segment/Items/HazardCrumbling.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class HazardCrumbling : MonoBehaviour
+{
+    [SerializeField] private bool CanRespawn = true;
+    [SerializeField] private float DelayBeforeFall = 0.5f;
+    [SerializeField] private float RespawnDelay = 3f;
+    [SerializeField] private string Tag_Player = "Player";
+    [SerializeField] private GameObject StonesRoot;
+
+    private bool _isTriggered;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_isTriggered) return;
+        if (!other.CompareTag(Tag_Player)) return;
+
+        _isTriggered = true;
+        CrumbleSequenceAsync().Forget();
+    }
+
+    private async UniTaskVoid CrumbleSequenceAsync()
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(DelayBeforeFall));
+
+        SetSolid(false);
+
+        if (!CanRespawn) return;
+
+        await UniTask.Delay(TimeSpan.FromSeconds(RespawnDelay));
+
+        SetSolid(true);
+        _isTriggered = false;
+    }
+
+    private void SetSolid(bool isSolid)
+    {
+        if (StonesRoot != null)
+        {
+            StonesRoot.SetActive(isSolid);
+        }
+    }
+}
