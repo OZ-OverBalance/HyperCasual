5745e38c9369b2a9714f8eb603e95a3356e347c3
이강호 <tiger04176672@gmail.com>
2026-08-25 14:01:04 +0900

    [feat] 배치 아이템 1종 추가 (컨베이어 벨트)

---
+++ Assets/Scripts/Features/Segment/Items/ObstacleConveyor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public enum ConveyorDirection
+{
+    Clockwise,     
+    CounterClockwise 
+}
+
+public class ObstacleConveyor : MonoBehaviour
+{
+    [SerializeField] private ConveyorDirection Direction_Belt = ConveyorDirection.Clockwise;
+    [SerializeField] private float BeltSpeed = 3f;
+    [SerializeField] private string Tag_Player = "Player";
+
+    //private bool _isActive;
+
+    //private void OnEnable()
+    //{
+    //    HazardActivationSignal.OnActivateAllRequested += HandleActivateAll;
+    //}
+
+    //private void OnDisable()
+    //{
+    //    HazardActivationSignal.OnActivateAllRequested -= HandleActivateAll;
+    //}
+
+    //private void HandleActivateAll()
+    //{
+    //    _isActive = true;
+    //}
+
+    private void OnTriggerStay(Collider other)
+    {
+        //if (!_isActive) return;
+        if (!other.CompareTag(Tag_Player)) return;
+
+        if (other.TryGetComponent(out CharacterController controller))
+        {
+            Vector3 pushDirection = GetPushDirection();
+            controller.Move(pushDirection * (BeltSpeed * Time.deltaTime));
+        }
+    }
+
+    private Vector3 GetPushDirection()
+    {
+        return Direction_Belt == ConveyorDirection.Clockwise ? Vector3.right : Vector3.left;
+    }
+}
