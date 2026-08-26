5fc1f13586abac798fad4e7f882389be7edaa65f
임현진 <hiimhoihoi@gmail.com>
2026-08-26 16:52:42 +0900

    [feat] 장치 인벤토리 UI 및 패널 설치 입력 차단
    - 지급된 장치 데이터에 따라 아이콘과 수량 표시
    - 선택 및 소진 상태에 따른 슬롯 UI 갱신
    - UI 위 포인터 입력 시 장치 설치 및 고스트 갱신 차단
    - 장치별 PlaceableObjectData 썸네일 연결

---
+++ Assets/Scripts/UI/Build/BuildInventoryRow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BuildInventoryRow : MonoBehaviour
+{
+    [SerializeField] private RectTransform RectTransform_Content;
+
+    public void AddSlot(BuildInventorySlot slot)
+    {
+        slot.transform.SetParent(RectTransform_Content, false);
+    }
+}
