ced112105667672853972c48cd5938328e076f52
김현빈 <khbkss77@naver.com>
2026-08-27 15:04:10 +0900

    [feat] 움직이는 오브젝트 동기화에 필요한 스크립트 틀 구현

---
+++ Assets/Scripts/Features/Segment/Base/ObstacleBase.cs
@@ -0,0 +1,53 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+public abstract class ObstacleBase : NetworkBehaviour
+{
+    protected bool HasStarted { get; private set; } = false;
+
+    protected virtual void Update()
+    {
+        if (NetCodeObstacleManager.Instance == null) return;
+
+        double startTime = NetCodeObstacleManager.Instance.GlobalStartTime.Value;
+
+        if (startTime <= 0)
+        {
+            if (HasStarted)
+            {
+                HasStarted = false;
+                OnObstacleStopped(); // 멈출 때 필요한 정리 작업 (선택사항)
+            }
+            return;
+        }
+
+        if (!HasStarted)
+        {
+            if (NetworkManager.Singleton.ServerTime.Time >= startTime)
+            {
+                HasStarted = true;
+                OnObstacleStarted();
+            }
+        }
+        else
+        {
+            OnObstacleUpdate();
+        }
+    }
+
+    /// <summary>
+    /// NetCodeObstacleManager에서 시작타이머가 시작된 초기에 실행할 메서드
+    /// </summary>
+    protected virtual void OnObstacleStarted() { }
+
+    /// <summary>
+    /// 시작타이머가 시작된 이후, 매 Update문 마다 실행할 메서드
+    /// </summary>
+    protected virtual void OnObstacleUpdate() { }
+
+    /// <summary>
+    /// 중간에 장애물시간을 멈추거나 페이즈가 끝났을때 실행할 메서드 
+    /// 필요시 오버라이드하여 사용
+    /// </summary>
+    protected virtual void OnObstacleStopped() { }
+}
