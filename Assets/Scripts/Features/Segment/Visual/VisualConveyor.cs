7399bf8919476ea72050c9afe7383431d5298bca
이강호 <tiger04176672@gmail.com>
2026-08-27 15:55:29 +0900

    [feat] 장애물 3종 연출 코드 추가

---
+++ Assets/Scripts/Features/Segment/Visual/VisualConveyor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class VisualConveyor : MonoBehaviour
+{
+    [SerializeField] private ObstacleConveyor Conveyor_Logic;
+    [SerializeField] private Renderer Renderer_Belt;
+    [SerializeField] private int MaterialSlotIndex = 1; 
+    [SerializeField] private float ScrollSpeed = 3f;
+    [SerializeField] private string TextureProperty = "_BaseMap_ST"; 
+
+    private MaterialPropertyBlock _propertyBlock;
+    private float _offset;
+
+    private void Awake()
+    {
+        _propertyBlock = new MaterialPropertyBlock();
+    }
+
+    private void Update()
+    {
+        if (Renderer_Belt == null || Conveyor_Logic == null) return;
+
+        float direction = Conveyor_Logic.Direction == ConveyorDirection.Clockwise ? 1f : -1f;
+        _offset += ScrollSpeed * direction * Time.deltaTime;
+
+        Renderer_Belt.GetPropertyBlock(_propertyBlock, MaterialSlotIndex);
+        _propertyBlock.SetVector(TextureProperty, new Vector4(1f, 1f, 0f, _offset));
+        Renderer_Belt.SetPropertyBlock(_propertyBlock, MaterialSlotIndex);
+    }
+}
