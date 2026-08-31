d1cf925820cf955faa7b846137d136d9c4195877
김현빈 <khbkss77@naver.com>
2026-08-31 15:40:57 +0900

    [feat] 두번째 BuildPhase 맵 생성 관련 수정
    BuildPhaseManager - 서버에서 받아온 데이터로 맵을 생성하게 수정
    RoundManager - 두번째 빌드페이즈로 진입하는 메서드 추가 (사용은 안함)

--- Assets/Scripts/Managers/BuildPhaseManager.cs
+++ Assets/Scripts/Managers/BuildPhaseManager.cs
@@ -148,7 +148,7 @@ public sealed class BuildPhaseManager : MonoBehaviour
         // 다음 라운드 빌드 페이즈 - 누적된 제작 맵 중 서버가 배정한 맵 사용. PlayerMapIds는 서버에서 동일한 순서로 동기화되어 전달되어야 함
         else
         {
-            if (!TryAssignAccumulatedMap(setupResult, localPlayerIndex))
+            if (!TryAssignAccumatedBuildMap(MapManager.Inst.CurrentBuildData))
             {
                 return;
             }
@@ -256,6 +256,33 @@ public sealed class BuildPhaseManager : MonoBehaviour
 
         Debug.Log($"BuildPhaseManager - 누적 맵 배정 결과 : {_assignedMapId}");
         return true;
+    }
+
+    private bool TryAssignAccumatedBuildMap(CraftMapData buildMapData)
+    {
+        if (!TryGetAssignedBuildMapId(buildMapData, out string mapId))
+        {
+            Debug.LogError("BuildPhaseManager - 누적 맵 배정 실패");
+            return false;
+        }
+
+        _assignedMapId = mapId;
+
+        Debug.Log($"BuildPhaseManager - 누적 맵 배정 결과 : {_assignedMapId}");
+        return true;
+    }
+
+    private bool TryGetAssignedBuildMapId(CraftMapData buildMapData, out string mapId)
+    {
+        mapId = string.Empty;
+
+        if (buildMapData == null || buildMapData.mapId == null)
+        {
+            return false;
+        }
+
+        mapId = buildMapData.mapId;
+        return !string.IsNullOrWhiteSpace(mapId);
     }
 
     private bool TryGetAssignedMapId(RoundMapSetupResult setupResult, int localPlayerIndex, out string mapId)
