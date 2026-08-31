f1ba06ebdce42cb610ac35d8521d58775c134243
김현빈 <khbkss77@naver.com>
2026-08-31 11:42:55 +0900

    [feat] RunPhase 끝나고 맵 삭제 추가

--- Assets/Scripts/Netcode/NetCodeMapManager.cs
+++ Assets/Scripts/Netcode/NetCodeMapManager.cs
@@ -59,6 +59,7 @@ public class NetCodeMapManager : NetworkBehaviour
     {
         NetCodeScoreManager.Instance.CalculationRoundScore();
         _arrivedPlayerIds.Clear();
+        MapManager.Inst.ClearAllMaps();
         PrintScoreLogForTestClientRpc();
     }
 
