a08faedde0895c946e437fd511b04396ace8d223
이강호 <tiger04176672@gmail.com>
2026-09-02 13:37:03 +0900

    [feat] 배치 아이템 1종 추가 (배치 아이템 제거 폭탄)

---
+++ Assets/Scripts/Features/Segment/Items/ObstacleBomb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class ObstacleBomb : MonoBehaviour
+{
+    [SerializeField] private float Lifetime = 1f;
+
+    public event Action OnDetonated;
+
+    private void Start()
+    {
+        OnDetonated?.Invoke();
+        Destroy(gameObject, Lifetime);
+    }
+}
